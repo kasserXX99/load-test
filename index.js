0c174fba82464dd493c83e94ed442a4ae1a90ad1
Amer <amershabebi@gmail.com>
2023-02-14 15:43:44 +0300

    adding the promise file

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 import { request } from "node:https";
 import { hrtime } from "node:process";
 
-const url = "https://jsonplaceholder.typicode.com/todos/";
-
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+const requestPassed = process.argv[2];
+const defaults = {
+	numberOfRequest: requestPassed || 10,
+};
+let counter = 0;
+let numberOfErrors = 0;
+let numberOfSuccess = 0;
 let arrOfTime = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < defaults.numberOfRequest; i++) {
 	let begining = hrtime.bigint();
-	let req = request(url + i, (res) => {
-		// console.log("a push of " + i);
+	let req = request(url, (res) => {
 		arrOfTime[i] = { begining };
 		res.setEncoding("utf8");
-
-		res.on("data", (data) => {
-			// arrOfTime.push({ begining, res, data: JSON.parse(data) });
-			// console.log("a request has been made" + i);
-			// console.log(data);
-		});
+		counter++;
+		res.on("data", (data) => {});
 		res.on("end", () => {
 			let ending = hrtime.bigint();
 			const toWholeNumber = Number(ending - arrOfTime[i].begining);
 			console.log(res.statusCode);
-			// we conver the bigint number to a whole number as bigint is not good with fractions we need fraction cause we might get miliseconds
-			// console.log(arrOfTime, arrOfTime.length, i);
-			// issue now is here as array size is not equivlant with i
+			if (res.statusCode >= 400 || res.statusCode >= 500) {
+				numberOfErrors++;
+			} else {
+				numberOfSuccess++;
+			}
+			// we convert the bigint number to a whole number as bigint is not good with fractions we need fraction cause we might get miliseconds
 			arrOfTime[i].ending = Number(toWholeNumber / 1e9).toFixed(3) + "s";
-			if (arrOfTime.length === 10) {
-				console.log(arrOfTime);
-				// arrOfTime.forEach((re) => {
-				// 	console.log(re.ending);
-				// });
+			if (counter === 10) {
+				console.log(
+					`you have ${numberOfErrors} of requests end with 4xx or 5xx`
+				);
+				console.log(`you have ${numberOfSuccess} of requests end with 2xx`);
+				let average = arrOfTime.reduce((a, b) => {
+					let removingTheS = b.ending.split("s");
+					return a + Number(removingTheS[0]);
+				}, 0);
+				console.log(
+					`the average time took for the request to be completed is ${Number(
+						average / defaults.numberOfRequest
+					).toFixed(3)}s`
+				);
 			}
 		});
 		req.on("error", (err) => {
@@ -38,5 +51,4 @@ for (let i = 0; i < 10; i++) {
 	req.end(() => {
 		console.log("the request has been ended");
 	});
-	// console.log(arrOfTime);
 }
